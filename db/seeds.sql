c85b1f42bf16a02a27fec7fb17722113dc227415
Patrick Neff <patrickneff@Patricks-MacBook-Air.local>
2018-08-21 21:01:40 -0600

    files done include: connection, controller, server, orm, db

---
+++ db/seeds.sql
@@ -0,0 +1,27 @@
+INSERT INTO burgers(
+    burger_name,
+)
+VALUES
+(
+    "Kraut burger"
+);
+
+INSERT INTO burgers(
+    burger_name,
+    devoured
+)
+VALUES
+(
+    "Royale with cheese",
+    True
+);
+
+INSERT INTO burgers(
+    burger_name,
+    devoured
+)
+VALUES
+(
+    "Veggie burger",
+    True
+);
